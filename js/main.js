6265900a70a71ba6744f52343c8e75229b1243b9
Amanda Chau <amandachau36@gmail.com>
2019-08-03 15:20:04 +1000

    Fixed links, footer and modals

---
+++ js/main.js
@@ -0,0 +1,1 @@
+console.log("Hi there! Thanks for checking out my console - see the code here https://github.com/amandachau36 and please let me know if you have any questions amandachau36@gmail.com :)")
